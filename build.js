36a60a555806b5e7a1a8d4377a32439858470323
Aidan Feldman <aidan.feldman@gmail.com>
2012-05-28 01:18:12 -0400

    first pass at exercise ordering

--- build.js
+++ build.js
@@ -3,12 +3,43 @@ var fs = require('fs');
 var template = fs.readFileSync('index.html.ejs');
 
 var dir = 'exercises/',
-  files = fs.readdirSync(dir);
+  allFiles = fs.readdirSync(dir);
+
+var files = [
+  'array_as_object.js',
+  'pass_by_reference.js',
+  'self_executing_functions.js', // re-referenced in modules
+
+  'this.js',
+  'apply_context.js',
+  'call_vs_apply.js',
+
+  'arguments.js',
+  'extend.js',
+
+  'event_loop.js',
+  'callbacks.js',
+
+  'object_literal.js',
+  'oop_basic.js',
+  'instance_vs_prototype.js',
+  'oop_inheritance.js',
+  'oop_private.js', // related to modules
+
+  'singleton.js',
+  'module.js'
+];
 
 var slides = files.map(function(file){
+  // remove from list of allFiles
+  var i = allFiles.indexOf(file);
+  if (i > -1) allFiles.splice(i, 1);
+
   var code = fs.readFileSync(dir + file);
   return '<dt></dt>\n<dd><pre>' + code + '</pre></dd>\n';
 }).join('');
+
+if (allFiles.length) console.warn('UNUSED FILES:\n' + allFiles.join('\n'));
 
 // console.log(template);
 var output = template.toString().replace(/<%=\s*examples\s*%>/, slides);
--- exercises/array_as_object.js
+++ exercises/array_as_object.js
@@ -11,4 +11,4 @@ assert(typeof myAry === 'object', "it's an object");
 
 myAry.newProp = "It's an object!";
 assert(myAry.newProp === "It's an object!", "assigning arbitrary properties");
-assert(myAry[0] === 'foo', "access with number, again");
+assert(myAry[0] === 'foo', "array access still works");
