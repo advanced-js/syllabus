d97b0dc4769945d58c508313d12ab26b469516a5
Aidan Feldman <aidan.feldman@gmail.com>
2013-04-27 12:10:58 -0400

    add more detailed namespace() instructions

--- examples/namespace_exercise.js
+++ examples/namespace_exercise.js
@@ -20,4 +20,20 @@ namespace('foo.lib.external');
 //   foo.lib = {};
 //   foo.lib.external = {};
 
+// Level 1: Make it work for namespaces
+//   of a certain length, e.g.
+//   'app.models' and 'foo.lib'.
+// 
+// Level 2: Make it work for namespaces
+//   of arbitrary length.
+// 
+// Level 3: If some level of the namespace
+//   already exists, don't override it, e.g.
+//   
+//   namespace('app.models');
+//   app.models.View = function(){};
+//   
+//   namespace('app.views');
+//   app.models.View; // should not raise an error
+
 // start here: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String/split
