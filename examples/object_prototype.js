4db110ab85ef26921d3a922ea966fab6fa12ee20
Aidan Feldman <aidan.feldman@gmail.com>
2013-10-05 13:10:23 -0400

    link to underscore isArray when discussing prototypes

--- examples/object_prototype.js
+++ examples/object_prototype.js
@@ -9,3 +9,6 @@ assert(typeof myAry === 'object', "so is an Array");
 
 assert(Object.prototype.toString.apply(myObj) === '[object Object]', "Object's toString() acts differently than typeof");
 assert(Object.prototype.toString.apply(myAry) === '[object Array]', "it can distinguish an Array from an Object");
+
+// look at _.isArray()
+// http://underscorejs.org/docs/underscore.html
