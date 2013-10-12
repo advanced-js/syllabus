7704a9964000af59b81662517a0a13da9483fe8d
Aidan Feldman <aidan.feldman@gmail.com>
2013-10-12 11:46:44 -0400

    add more clarification in the namespace slides

--- examples/namespace.js
+++ examples/namespace.js
@@ -1,10 +1,14 @@
 // like Java, but hackier!
 
-var app = {};
+// create the namespace
+var app = {}; // the only global
 app.models = {};
+
+// add stuff to the namespace
 app.models.User = function(){};
 app.models.Vehicle = function(){};
 
+// use stuff from the namespace
 var bob = new app.models.User();
 
 assert(bob instanceof Object, "Objects under a namespace are just Objects");
