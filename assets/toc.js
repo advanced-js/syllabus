bc8f47db0aa6105aa9d144e70030859c8ce08655
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-21 15:06:09 -0500

    switch to generating the TOC manually

--- assets/toc.js
+++ assets/toc.js
@@ -1,5 +1,39 @@
+// inspiration:
+// * https://jsfiddle.net/gableroux/S2SMK/
+// * http://gregfranko.com/jquery.tocify.js/
 $(function() {
-  $('#toc').tocify({
-    selectors: 'h2, h3, h4'
+  var $toc = $('#toc');
+
+  // essentially a media query for Bootstrap's col-sm-* breakpoint – make the table of contents "sticky" on wider screen sizes
+  if ($(window).width() >= 768) {
+    $toc.addClass('sticky');
+  }
+
+  var getAnchor = function(el) {
+    if (el.id) {
+      return el.id;
+    } else {
+      var text = $(el).text();
+      return text.toLowerCase().replace(/\s+/, '-').replace(/[^\w\-]+/, '');
+    }
+  };
+
+  var $context = $('<ul></ul>');
+  $toc.append($context);
+
+  var $prevNav;
+  var prevNavLevel;
+
+  $('h2,h3,h4').each(function(i, el) {
+    var anchor = getAnchor(el);
+    var text = $(el).text();
+
+    var $newNav = $('<li><a href="#' + anchor + '">' + text + '</a></li>');
+
+    $context.append($newNav);
+
+    $prevNav = $newNav;
   });
+
+  $('.sticky').Stickyfill();
 });
