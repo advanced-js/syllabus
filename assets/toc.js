1b298d2c87b7608acb6b332d75dc627b3b908e2c
Aidan Feldman <aidan.feldman@gsa.gov>
2015-11-21 16:09:16 -0500

    handle nested tables of contents

--- assets/toc.js
+++ assets/toc.js
@@ -21,28 +21,48 @@ $(function() {
   var $context = $('<ul></ul>');
   $toc.append($context);
 
-  var $prevNav;
-  var prevNavLevel;
-
-  $('h2,h3,h4').each(function(i, el) {
+  $('h1,h2,h3,h4').each(function(i, el) {
     var anchor = getAnchor(el);
     var text = $(el).text();
-    var $newNav = $('<li><a href="#' + anchor + '">' + text + '</a></li>');
 
+    var $newNav = $('<li><a href="#' + anchor + '">' + text + '</a></li>');
     var navLevel = parseInt(el.tagName.charAt(1), 10);
-    if (prevNavLevel) {
-      if (navLevel > prevNavLevel) {
-        var $childList = $('<ul></ul>');
-        $prevNav.append($childList);
-        $context = $childList;
-      } else if (navLevel < prevNavLevel) {
-        $context = $($context.parent());
+    $newNav.data('nav-level', navLevel);
+
+    // adjust the context
+    var i = 0;
+    // `i` is just a safety valve, so we don't accidentally get an endless loop
+    while (i < 10) {
+      var prevNav = $context.find('li:last')[0];
+      if (prevNav) {
+        var $prevNav = $(prevNav);
+        var prevNavLevel = $prevNav.data('nav-level');
+        if (navLevel > prevNavLevel) {
+          // create a new level of the tree
+          var $childList = $('<ul></ul>');
+          $prevNav.append($childList);
+          $context = $childList;
+        } else if (navLevel < prevNavLevel) {
+          // walk up the tree
+          var $parentContext = $context.parent().parent('ul');
+          if ($parentContext.length) {
+            $context = $parentContext;
+          } else {
+            // we've reached the top-level element
+            break;
+          }
+        } else {
+          // matching level
+          break;
+        }
+      } else {
+        // no previous element
+        break;
       }
+
+      i++;
     }
     $context.append($newNav);
-
-    $prevNav = $newNav;
-    prevNavLevel = navLevel;
   });
 
   $('.sticky').Stickyfill();
